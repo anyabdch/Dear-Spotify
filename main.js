330b927859ec8bfc592696d727808dd4aa44d589
Anya <116769112+anyabdch@users.noreply.github.com>
2023-02-08 14:47:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,45 +15,66 @@ var playlists = [
     },
     {
         img: "wi.png",
-        desc: "I'm an experimenter at heart. I'm guilty of having like 10 different types of 'progress' photo albums on my phone to see if I've changed \
+        desc: "I'm a data collector at heart. I'm guilty of having like 10 different types of 'progress' photo albums on my phone to see if I've changed \
                 over time. My Spotify is not immune to impulsive data collection. My latest idea has been to have playlists for every season and whenever \
                 I add a song to my library or get a song stuck in my head, I also put it in the season playlist for whatever time of year it is. I want to \
-                find out if there's a certain vibe for each season that naturally develops, and have fun little caches of seasonal memories.",
+                find out if there's a certain vibe for each season that naturally develops, and have fun little caches of seasonal memories. Enjoy this \
+                season's collection!",
         embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/12bkxiqORMdwp0TxdHqpLy?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "es.png",
-        desc: "",
+        desc: "Growing up, I lived in a two language household. My father, who lived in Mexico for a long time, only speaks to my younger sister and I in \
+                Spanish and I was on a dual-language track throughout high school. Being bilingual is a huge part of my identity, which many find ironic because \
+                ethnically I'm Ashkenazi Jewish and Italian. I often get asked '¿eres Española?' -- 'are you Spanish?' -- from people trying to justify my \
+                accent with the color of my skin. So this playlist was born! Some of my favorite spanish music (I would kill to see Bad Bunny in concert) \
+                and the playlist I work out to :)"
         embed: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0oa6tevkiM8XlRaL6ffBcQ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "qugast.png",
-        desc: "",
+        desc: "My study playlist! Before coming to college, you couldn't pay me money to listen to lyricless music. If it's an acquired taste I've certainly \
+                acquired it in the last couple years. This playlist has gotten me through countless late finals week nights, 10 page papers, major projects, \
+                and application processes. It's what I'm listening to while I make this :) In my opinion, one of my greatest skills is with this playlist \
+                blasting, I can work in quite literally any setting.",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1o565KfcNpmEIkbX06jdXK?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "fa.png",
-        desc: "",
+        desc: "(See the Winter playlist for more context) This is currently my favorite of my seasonal playlists. I grew up in sunny SoCal, so I had never \
+                experienced fall colors before coming to Northwestern and I find it genuinely magical. While I'm still a California girl at heart (and would \
+                love to work somewhere like Spotify that would enable me to work at an office in my home state), I don't think I could fully leave the \
+                foliage behind. My dream is to spend some time in Seattle to be near the Olympic Peninsula. More specifically my dream is to hike the Pacific \
+                Crest Trail from Los Angeles to Seattle and arrive just in time for fall foliage and the autumn hiking season, but that's a story for another \
+                time :)",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/51gyD1SzsAxdTSeeZfVVPO?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "sccrwi.png",
-        desc: "",
+        desc: "Need I say more?",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/68fT60Q8GgCkYPY6aw8s9c?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "patn.png",
-        desc: "",
+        desc: "Spotify has a knack for bringing people together. I work as an Orientation Leader (we're called Peer Advisers or PAs) at Northwestern, where each \
+                PA is assigned a group of 10-15 incoming students to guide through orientation week (we call it Wildcat Welcome). I had the, ahem, luck of having \
+                a group that consisted of 11 very unenthusiastic boys and one girl. I had a hard time getting them to open up at first, so I had to get creative \
+                with it. I created this playlist with each of their favorite songs from our intake spreadsheet, and told them I'd be blasting this and this alone \
+                for all of Wildcat Welcome so they'd better 'make it snazzy'. Within an hour they had contributed 4 hours worth of music and we're starting to \
+                mingle. I've proven I can think outside the box with Spotify, now I want to think outside the box <i>at</i> Spotify ;)",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6r3CmZR6YjVuthveWqwGn2?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "titosc.png",
-        desc: "",
+        desc: "This isn't actually my playlist, as you can probably tell from the title and genre. My boyfriend made this for a 4am drive to a hike that he took my \
+                mom and I on. Spotify proved itself to be a fabulous bonding tool and my mom proved itself to be a hell of a headbanger. When I told him I was \
+                including this playlist in my project he was actually concerned it would prevent me from getting a job for his taste in music. Prove him wrong!",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7xFZ2ylghoz9ioZrYgFaVp?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     },
     {
         img: "csre.png",
-        desc: "",
+        desc: "Last but far, far from least, this playlist is the song recommendations my first Spotify API algorithm in my very first CS course gave me. Though I \
+                don't listen to it often, it's a reminder of where I started and where I want to go <3",
         embded: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2t9Io6PdiEDfExHFhsCn3d?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
     }
 ]
